8725276eba257ba64d8a8097ac362f914a1a718b
Matthew Carmichael <sealblox@gmail.com>
2025-05-07 15:29:56 +0000

    Added URL paths

--- game/urls.py
+++ game/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('dungeon/', views.dungeon),
+    path('gameplay/', views.gameplay),
 ]
     
--- game/views.py
+++ game/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main.html', {})
-    
+
+def dungeon(request):
+    return render(request, 'dungeon.html', {})
+
+def gameplay(request):
+    return render(request, 'gameplay.html', {})
